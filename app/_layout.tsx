0dbf3f125da8c59e8aaf89fed040883df1773177
Tolulope Malomo <malomo.tolz@gmail.com>
2025-03-23 16:45:43 +0100

    init

---
+++ app/_layout.tsx
@@ -0,0 +1,34 @@
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "../theme";
+
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colorGreen,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/profile.tsx
@@ -0,0 +1,19 @@
+import { theme } from "@/theme";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Profile() {
+  return (
+    <View style={styles.container}>
+      <Text>Profile</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
